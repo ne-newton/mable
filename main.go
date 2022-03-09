20eceef4f0f1b31a14df4398ec840a1122313d35
Neil Newton <neil.newton@rice.edu>
2022-03-09 15:05:56 -0800

    Altered structs, addBookVersion, and removedBookVersion, but still in progress

--- main.go
+++ main.go
@@ -9,54 +9,96 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"time"
 )
 
-type ApprovedVersion struct {
-	CollectionID   string `json:"collection_id"`
-	ContentVersion string `json:"content_version"`
+type Book struct {
+	Style string `json:"style"`
+	UUID  string `json:"uuid"`
+	Slug  string `json:"slug"`
+}
+
+type Version struct {
 	MinCodeVersion string `json:"min_code_version"`
+	Edition        int    `json:"edition"`
+	CommitSha      string `json:"commit_sha"`
+	CommitMetadata struct {
+		CommittedAt time.Time `json:"committed_at"`
+		Books       []Book `json:"books"`
+	} `json:"commit_metadata"`
 }
 
 type ABL struct {
+	APIVersion    int `json:"api_version"`
 	ApprovedBooks []struct {
-		CollectionID string `json:"collection_id"`
-		Server       string `json:"server"`
-		Style        string `json:"style"`
-		TutorOnly    bool   `json:"tutor_only"`
-		Books        []struct {
-			UUID string `json:"uuid"`
-			Slug string `json:"slug"`
-		} `json:"books"`
+		RepositoryName string    `json:"repository_name"`
+		Platforms      []string  `json:"platforms"`
+		Versions       []Version `json:"versions"`
 	} `json:"approved_books"`
-	ApprovedVersions []ApprovedVersion `json:"approved_versions"`
+	ApprovedVersions []interface{} `json:"approved_versions"`
 }
 
-//will remove the first book in the ABL that matches both collectionID and contentVersion
-func (a *ABL) removeBookVersion(collectionID, contentVersion string) {
-	for i, v := range a.ApprovedVersions {
-		if v.CollectionID == collectionID && v.ContentVersion == contentVersion {
+//will remove the first book in the ABL that matches both repo, commit sha, and book slug
+func (a *ABL) removeBookVersion(repositoryName, commitSha, slug string) {
+	for i, ab := range a.ApprovedBooks {
+		if ab.RepositoryName == repositoryName {
+			for j, v := range a.ApprovedBooks[i].Versions {
+				if v.CommitSha == commitSha {
+					for k, b := range a.ApprovedBooks[i].Versions[j].CommitMetadata.Books {
+						if b.Slug == slug {
+							Books := a.ApprovedBooks[i].Versions[j].CommitMetadata.Books
+							if len(Books) > 0 {
+								a.ApprovedBooks[i].Versions[j].CommitMetadata.Books = append(Books[:k], Books[:k+1]...)
+							} else {
+								Versions := a.ApprovedBooks[i].Versions
+								a.ApprovedBooks[i].Versions = append(Versions[:j], Versions[:j+1]...)
+							}
+						}
+					}
+				}
+			}
 			a.ApprovedVersions = append(a.ApprovedVersions[:i], a.ApprovedVersions[i+1:]...)
-			fmt.Printf("%s %s was removed from the ABL\n", collectionID, contentVersion)
+			fmt.Printf("%s %s of %s was removed from the ABL\n", slug, commitSha, repositoryName)
 			return
 		}
 	}
-	log.Fatalf("%s %s could not be found, nothing was removed from the ABL\n", collectionID, contentVersion)
+	log.Fatalf("%s %s of %s could not be found, nothing was removed from the ABL\n", slug, commitSha, repositoryName)
 }
 
-//checks to see if version already in ABL, if not, adds it
-func (a *ABL) addBookVersion(collectionID, contentVersion, minCodeVersion string) {
+//still needs to have version info added
+//checks to see if books and version already exists, if the version exists, but not the book, it addes the book to the version
+//if the version doesn't exist, it adds both. If both do exist, it creates an error
+func (a *ABL) addBookVersion(repositoryName, commitSha, style, uuid, slug string) {
 	exists := false
-	for _, v := range a.ApprovedVersions {
-		if v.CollectionID == collectionID && v.ContentVersion == contentVersion {
-			exists = true
+	abCount := 0
+	vCount := 0
+	for i, ab := range a.ApprovedBooks {
+		if ab.RepositoryName == repositoryName {
+			for j, v := range a.ApprovedBooks[i].Versions {
+				if v.CommitSha == commitSha {
+					for _, b := range a.ApprovedBooks[i].Versions[j].CommitMetadata.Books {
+						if b.Slug == slug {
+							exists = true
+						} else {
+							abCount = i
+							vCount = j
+						}
+					}
+				}
+			}
 		}
 	}
 	if exists == false {
-		a.ApprovedVersions = append(a.ApprovedVersions, ApprovedVersion{CollectionID: collectionID, ContentVersion: contentVersion, MinCodeVersion: minCodeVersion})
-		fmt.Printf("%s %s has been added to the ABL", collectionID, contentVersion)
+		a.ApprovedBooks[abCount].Versions[vCount].CommitMetadata.Books = append(a.ApprovedBooks[abCount].Versions[vCount].CommitMetadata.Books, Book{Style: style, UUID: uuid, Slug: slug})
+		fmt.Printf("%s %s of %s has been added to the ABL", slug, commitSha, repositoryName)
 	} else {
-		log.Fatalf("%s %s already exists in the ABL", collectionID, contentVersion)
+		log.Fatalf("%s %s of %s already exists in the ABL", slug, commitSha, repositoryName)
 	}
+}
+
+//adds an entirely new books to the ABL, but not a version. A version needs to be added after this step.
+func (a *ABL) addNewBook() {
+
 }
 
 //fetches most recent ABL, unmarshalls json to struct
@@ -86,20 +128,7 @@ func fetchABL() ABL {
 	return abl
 }
 
-//func uploadABL(abl ABL) {
-//	ablJson, errJson := json.Marshal(abl)
-//	if errJson != nil {
-//		log.Fatal("ABL could not convert to JSON:", errJson)
-//	}
-//	t, fileErr := ioutil.ReadFile("token.txt")
-//	if fileErr != nil {
-//		log.Fatal("error reading token:", fileErr)
-//	}
-//	ctx := context.Background()
-//	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: string(t)})
-//	tc := oauth2.NewClient(ctx, ts)
-//	client := github.NewClient(tc)
-//}
+//needs to be updated for newest version of ABL
 
 //checks if cl arguments are valid
 func argsError(args []string) bool {
@@ -159,10 +188,7 @@ func writeABL(abl ABL) {
 	}
 }
 
-func pushABL() {
-	fmt.Println("this doesn't work yet")
-}
-
+//flags need to be updated
 func main() {
 	var abl ABL
 	removePtr := flag.Bool(
@@ -180,11 +206,6 @@ func main() {
 		false,
 		"download the most recent version of the approved-book-list.json: \"./mable -update\"",
 	)
-	pushPtr := flag.Bool(
-		"push",
-		false,
-		"push local version of approved-book-list.json to github: \"./mable -push\"",
-	)
 	countPtr := flag.Bool(
 		"count",
 		false,
@@ -193,21 +214,21 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	if *removePtr {
-		if len(args) != 2 {
-			log.Fatal("error: \"-remove\" requires 2 arguments, collection ID and content version")
+		if len(args) != 3 {
+			log.Fatal("error: \"-remove\" requires 3 arguments, repository name, commit SHA, and slug")
 		}
 		if !argsError(args) {
 			abl = loadABL()
-			abl.removeBookVersion(args[0], args[1])
+			abl.removeBookVersion(args[0], args[1], args[2])
 			writeABL(abl)
 		}
 	} else if *addPtr {
-		if len(args) != 3 {
-			log.Fatal("error: \"-add\" requires 3 arguments, collection ID, content version, and min code version")
+		if len(args) != 5 {
+			log.Fatal("error: \"-add\" requires 5 arguments, repository name, commit SHA, style, Book UUID, and slug")
 		}
 		if !argsError(args) {
 			abl = loadABL()
-			abl.addBookVersion(args[0], args[1], args[2])
+			abl.addBookVersion(args[0], args[1], args[2], args[3], args[4])
 			writeABL(abl)
 		}
 	} else if *countPtr {
@@ -217,9 +238,6 @@ func main() {
 		abl = fetchABL()
 		fmt.Println("most recent version of approved-book-list.json has been downloaded")
 		writeABL(abl)
-	} else if *pushPtr {
-		abl = loadABL()
-		pushABL()
 	} else {
 		fmt.Println("MABLE: Making the ABL Easier\nrun \"./mable -h\" for help")
 	}
